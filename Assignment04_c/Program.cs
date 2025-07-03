0b4a34ec428783238e6e6da3338a05f3c3ec22e5
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-07-03 09:52:19 +0300

    p6

--- Assignment04_c/Program.cs
+++ Assignment04_c/Program.cs
@@ -26,13 +26,140 @@ namespace Assignment04_c
             //people[1] = new person("mostafa", 20);
             //people[2] = new person("sayed", 22);
 
-   
+
             //Console.WriteLine("List of Persons:");
             //foreach (person p in people)
             //{
             //    p.display();
             //}
             #endregion
+
+
+            #region p3:Create an enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
+
+            //Console.WriteLine("Enter a season (Spring, Summer, Autumn, Winter):");
+            //string input = Console.ReadLine()!;
+
+
+            //if (Enum.TryParse(input, true, out Season season))
+            //{
+
+            //    switch (season)
+            //    {
+            //        case Season.Spring:
+            //            Console.WriteLine("Spring: March to May");
+            //            break;
+            //        case Season.Summer:
+            //            Console.WriteLine("Summer: June to August");
+            //            break;
+            //        case Season.Autumn:
+            //            Console.WriteLine("Autumn: September to November");
+            //            break;
+            //        case Season.Winter:
+            //            Console.WriteLine("Winter: December to February");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid season.");
+            //            break;
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Enter a valid season.");
+            //}
+            #endregion
+
+
+            #region p4: Create an enum called "Colors" with the basic colors(Red, Green, Blue) as its members.Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
+
+            //Console.WriteLine("Enter a color (Red, Green, Blue):");
+            //string input = Console.ReadLine()!;
+
+
+            //if (Enum.TryParse(input, true, out Colors color))
+            //{
+            //    if (Enum.IsDefined(typeof(Colors), color))
+            //    {
+            //        Console.WriteLine($"{color} is a primary color.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{color} is not a primary color.");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" Enter a valid color.");
+            //}
+
+
+            #endregion
+
+
+            #region p5:Create a struct called "Point" to represent a 2D point with properties "X" and "Y". Write a C# program that takes two points as input from the user and calculates the distance between them.
+
+            //{
+
+            //    Console.WriteLine("Enter of the first point (X Y):");
+            //    string[] point1Input = Console.ReadLine()!.Split();
+            //    double x1 = Convert.ToDouble(point1Input[0]);
+            //    double y1 = Convert.ToDouble(point1Input[1]);
+
+
+            //    Point point1 = new Point(x1, y1);
+
+
+            //    Console.WriteLine("Enter of the second point (X Y):");
+            //    string[] point2Input = Console.ReadLine()!.Split();
+            //    double x2 = Convert.ToDouble(point2Input[0]);
+            //    double y2 = Convert.ToDouble(point2Input[1]);
+
+            //    Point point2 = new Point(x2, y2);
+
+
+            //    double distance = point1.Distance(point2);
+
+
+            //    Console.WriteLine($"The distance between the two points is: {distance}");
+
+
+            #endregion
+
+
+            #region p6: Create a struct called "Person" with properties "Name" and "Age". Write a C# program that takes details of 3 persons as input from the user and displays the name and age of the oldest person.
+
+            //Person[] persons = new Person[3];
+
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    Console.WriteLine($"Enter details for person {i + 1}:");
+            //    Console.Write("Name: ");
+            //    string name = Console.ReadLine()!;
+            //    Console.Write("Age: ");
+            //    int age;
+            //    while (!int.TryParse(Console.ReadLine(), out age) || age <= 0)
+            //    {
+            //        Console.Write(" Please enter a valid age: ");
+            //    }
+
+            //    persons[i] = new Person(name, age);
+
+            //}
+
+            //Person oldest_person = persons[0];
+
+            //for (int j = 1; j < persons.Length; j++)
+            //{
+            //    if (persons[j].Age > oldest_person.Age)
+            //    {
+            //        oldest_person = persons[j];
+            //    }
+            //}
+
+            //Console.WriteLine("\n The oldest person is:");
+            //Console.WriteLine($"Name: {oldest_person.Name}, Age: {oldest_person.Age}");
+            #endregion
+
         }
     }
 }
--- shared/Class1.cs
+++ shared/Class1.cs
@@ -20,9 +20,40 @@
 
         public void display()
         {
-            Console.WriteLine($"Name: {Name}   ,Age: {Age}");
+            Console.WriteLine($"Name: {Name}   ,\n Age: {Age}");
         }
     }
+
+    public struct Point
+    {
+        public double X;
+        public double Y;
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public double  Distance(Point other)
+        {
+            double deltaX = X - other.X;
+            double deltaY = Y - other.Y;
+
+            return (int)Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+    }
+
+    public struct Person
+    {
+        public string Name;
+        public int Age;
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+      
+    }
+      
 
     public enum WeekDays
     {
@@ -33,5 +64,23 @@
         Friday,
         Saturday,
         Sunday
+    }
+
+
+    public enum Season
+    {
+        Spring,
+        Summer,
+        Autumn,
+        Winter
+    }
+
+
+    public enum Colors
+    {
+        Red,
+        Green,
+        Blue,
+        
     }
 }
