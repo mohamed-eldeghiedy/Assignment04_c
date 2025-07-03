ad97559525e1fea4d51a7327ce08c02bc702e100
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-07-03 04:21:41 +0300

    Add project files.

---
+++ Assignment04_c/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using shared;
+namespace Assignment04_c
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+            Console.WriteLine("Days of the Week: ");
+            foreach (WeekDays day in Enum.GetValues(typeof(shared.WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
+        }
+    }
+}
---
+++ shared/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace shared
+{
+    public class Class1
+    {
+
+    }
+
+    public enum WeekDays
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
